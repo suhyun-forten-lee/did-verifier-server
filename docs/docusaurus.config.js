daaf86ac1bfb38b4a0d39b38d252cb51f1fbe270
suhyun-forten-lee <nine01223@kakao.com>
2024-11-20 02:55:50 +0900

    docs: test

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -49,7 +49,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/suhyun-forten-lee/did-issuer/server/edit/main/',
+            'https://github.com/suhyun-forten-lee/did-verifier-server/edit/main/docs/',
         },
         blog: {
           showReadingTime: true,
@@ -60,7 +60,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/suhyun-forten-lee/did-issuer/server/edit/main/',
+            'https://github.com/suhyun-forten-lee/did-verifier-server/edit/main/docs/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
